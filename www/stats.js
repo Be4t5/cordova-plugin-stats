f9c09f7cb297d866ee1b630800d42cb583d6748f
Be4t5 <thomas.traverso54@gmail.com>
2018-09-07 20:10:38 +0200

    1

--- www/stats.js
+++ www/stats.js
@@ -1,9 +1,15 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
+    initElephant: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Stats", "initElephant", [name]);
         cordova.exec(successCallback, errorCallback, "Stats", "consentElephant", [name]);
+        cordova.exec(successCallback, errorCallback, "Stats", "revokeElephant", [name]);
+    }
+	consentElephant: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Stats", "consentElephant", [name]);
+    }
+	revokeElephant: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Stats", "revokeElephant", [name]);
     }
 };
