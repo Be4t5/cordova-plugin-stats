ed0713c488f795b658918ab853b208e7e8595211
Be4t5 <thomas.traverso54@gmail.com>
2018-09-26 22:16:42 +0200

    2

--- src/android/Stats.java
+++ src/android/Stats.java
@@ -4,7 +4,7 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import com.geouniq.android.*;
-import com.google.*;
+import com.google.gson.Gson;
 import com.elephant.data.ElephantLib;
 
 
