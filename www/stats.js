a4e309417c825acc724e6377cae4865d6942afcd
Be4t5 <thomas.traverso54@gmail.com>
2019-01-26 12:18:46 +0100

    Update stats.js

--- www/stats.js
+++ www/stats.js
@@ -10,17 +10,4 @@ module.exports = {
 	revokeElephant: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Stats", "revokeElephant", [name]);
     }
-	,
-	initGeoUniq: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Stats", "initGeoUniq", [name]);
-    },
-	consentGeoUniq: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Stats", "consentGeoUniq", [name]);
-    },
-	revokeGeoUniq: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Stats", "revokeGeoUniq", [name]);
-    },
-	checkGeoUniq: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Stats", "checkGeoUniq", [name]);
-    }
 };
